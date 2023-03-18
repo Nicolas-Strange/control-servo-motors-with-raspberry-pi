43921ddaa5637891feca6a3102968a4f8e2c546f
wataiso <wataiso@gmail.com>
2023-03-18 14:02:57 +0900

    wip

--- core_run_raspberry_pi/main.py
+++ core_run_raspberry_pi/main.py
@@ -11,6 +11,9 @@ class Main:
     FILE_NAME = "data_rotation_results"
     SERVO_NAME = "servo_sg9"
 
+    min_val_inc = -90
+    max_val_inc = 90
+
     def __init__(self):
         """
         init function
@@ -30,36 +33,46 @@ class Main:
 
         GPIO.setup(self._gpio_photo_intercept, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    def _run(self, percent_speed: float) -> None:
+        """ run one epoch """
+        start_time = time_ns()
+
+        waiting_time, step = self._servo.go_to_position(angle=self.max_val_inc, percent_speed=percent_speed)
+
+        # While the IR sensor is not activated we wait
+        while not GPIO.input(self._gpio_photo_intercept):
+            sleep(0.001)
+
+        rotation_time = (time_ns() - start_time) / (10 ** 9)
+        rotation_speed = 180 / rotation_time
+
+        self._append_file(f"{percent_speed},{rotation_speed}")
+
+        print(f"rotation_speed(°/s): {rotation_speed} -- "
+              f"step: {step} -- waiting_time(s) {waiting_time}")
+
+        self._init_position()
+
     def run(self) -> None:
         """
         core function to iterate
         For each iteration the motion value will be read
         """
         try:
-            self._servo.go_to_position(angle=-90, percent_speed=100)
-            sleep(1)
+            self._init_position()
             for percent_speed in range(0, 110, 10):
-                start_time = time_ns()
-                waiting_time, step = self._servo.go_to_position(angle=90, percent_speed=percent_speed)
-
-                # While the IR sensor is not activated we wait
-                while not GPIO.input(self._gpio_photo_intercept):
-                    sleep(0.001)
-
-                rotation_time = (time_ns() - start_time) / (10 ** 9)
-
-                print(f"percent_speed: {percent_speed} --- Rotation speed (°/s): {180 / rotation_time} "
-                      f"-- waiting_time: {waiting_time} -- step: {step}")
-
-                self._append_file(f"{percent_speed},{180 / rotation_time}")
-                sleep(1)
-                self._servo.go_to_position(angle=-90, percent_speed=100)
-                sleep(1)
+                self._run(percent_speed=percent_speed)
 
         except KeyboardInterrupt:
             self._servo.release()
 
         self._servo.release()
+
+    def _init_position(self):
+        """ initialize the servo position """
+        sleep(1)
+        self._servo.go_to_position(angle=self.min_val_inc, percent_speed=100)
+        sleep(1)
 
     def _append_file(self, value: str) -> None:
         """ write in a file: append mode """
